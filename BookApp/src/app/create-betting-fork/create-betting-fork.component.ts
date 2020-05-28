a0e88a2d2082a24f73f8f1c3a73a2d85b208d0ac
tbl4h <mic.maci2891@gmail.com>
2020-05-28 20:29:39 +0200

    Create Reactive data in betting fork component

--- BookApp/src/app/create-betting-fork/create-betting-fork.component.ts
+++ BookApp/src/app/create-betting-fork/create-betting-fork.component.ts
@@ -1,6 +1,7 @@
 import { BettingForkData } from './../MockFiles/bettingFork';
 import { ComputationServicesModule } from './../computation-services/computation-services.module';
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-create-betting-fork',
@@ -9,8 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateBettingForkComponent implements OnInit {
 
-  BettingForkData;
-
+  /* Useful in template form type */
+  // BettingForkData;
+  
+  /* Reactive form Data */
+  startingValue = new FormControl(0);
+  expectedValue = new FormControl(0);
+  wholeBudget = new FormControl(0);
+  minProbabilityPerCombination = new FormControl(0);
+  minProfitPerCombination = new FormControl(0);
+  minSucceedDraw  = new FormControl(0);
+  
   constructor() { }
 
   ngOnInit(): void {
